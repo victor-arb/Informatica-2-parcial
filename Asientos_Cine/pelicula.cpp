4cf91abebd7e459d1f92ec3efe1945440843f6ed
victor-arb <62657690+victor-arb@users.noreply.github.com>
2020-05-09 01:13:28 -0500

    Clase Pelicula modificada

--- Asientos_Cine/pelicula.cpp
+++ Asientos_Cine/pelicula.cpp
@@ -1,6 +1,178 @@
 #include "pelicula.h"
 
-Pelicula::Pelicula()
+Pelicula::Pelicula(string _nombre, string _genero, int _sala, string _hora, int _nfil, int _ncol, string _clasificacion, string _formato)
+{
+    nombre = _nombre;
+    genero = _genero;
+    sala = _sala;
+    hora = _hora;
+    nfil = _nfil;
+    ncol = _ncol;
+    setAsientTotal();                       //Crea los asientos totales
+    asien_disponible = asien_total;         //Asientos totales = filas * columnas
+    setAsientosSala();                      //Crea la sala con los asientos en modo disponibles;
+    clasificacion = _clasificacion;
+    formato = _formato;
+}
+Pelicula::~Pelicula()
 {
 
 }
+
+//Metodos setters
+void Pelicula::setNombre(string _nombre)
+{
+    nombre = _nombre;
+}
+void Pelicula::setGenero(string _genero)
+{
+    genero = _genero;
+}
+void Pelicula::setSala(int _sala)
+{
+    sala = _sala;
+}
+void Pelicula::setHora(string _hora)
+{
+    hora = _hora;
+}
+void Pelicula::setAsientDisponible(int _asien_disponible)
+{
+    asien_disponible = _asien_disponible;
+}
+void Pelicula::setAsientTotal()
+{
+   asien_total = nfil*ncol;
+}
+void Pelicula::setClasificacion(string _clasificacion)
+{
+    clasificacion = _clasificacion;
+}
+void Pelicula::setFilas_Colum(int _nfil, int _ncol)
+{
+    nfil = _nfil;
+    ncol = _ncol;
+}
+
+//Crea los asientos en modo disponible, con el caracter "+"
+void Pelicula::setAsientosSala()
+{
+    asientos_sala = new string*[nfil];
+    for (int i = 0; i < nfil; i++) {
+        asientos_sala[i] = new string[ncol];
+    }
+
+    //Llena la matriz
+    for (int i = 0; i < nfil; i++) {
+        for (int j=0; j< ncol; j++) {
+            asientos_sala[i][j] = "+";
+        }
+    }
+}
+
+//Metodos getters
+string Pelicula::getNombre()
+{
+    return nombre;
+}
+string Pelicula::getGenero()
+{
+    return genero;
+}
+string Pelicula::getDuracion()
+{
+    return duracion;
+}
+int Pelicula::getSala()
+{
+    return sala;
+}
+string Pelicula::getHora()
+{
+    return hora;
+}
+int Pelicula::getAsientDisponible()
+{
+    return asien_disponible;
+}
+int Pelicula::getAsientTotal()
+{
+    return asien_total;
+}
+string Pelicula::getClasificacion()
+{
+    return clasificacion;
+}
+string Pelicula::getFormato()
+{
+    return formato;
+}
+int Pelicula::getFila()
+{
+    return nfil;
+}
+int Pelicula::getColumna()
+{
+    return ncol;
+}
+
+//Mostrar la sala con los asientos disponibles
+void Pelicula::showSala()
+{
+    string aux(unsigned(ncol)*7, '-');
+    cout<<aux<<endl;
+    cout<<"   ";
+    for (int i = 1; i <= ncol; i++) {
+        if (i < 10){
+            cout<<" |   "<<i;
+        }
+        else if (i == 10) cout << " | "<<i;
+        else if (i<100) {
+            cout<<"  | "<<i;
+        }
+    }
+    cout<< "  |"<<endl;
+    cout<<aux<<endl;
+    string auxLetra = "A";
+    auxLetra[0] += (nfil-1);        //Variable auxiliar para imprimir la letra a la que pertenece la fila
+    for (int i = 0; i<nfil ; i++) {
+        cout <<" "<<auxLetra<<" |";
+        for (int j = 0; j < ncol; j++) {
+            if (j==0){
+                cout<<"|  "<<asientos_sala[i][j];
+            }
+            else {
+                cout<<"  |  "<<asientos_sala[i][j];
+            }
+
+        }
+        cout<< "  || "<<auxLetra<<" |"<<endl;
+        cout<<aux<<endl;
+        auxLetra[0] -= 1;
+    }
+    cout<<"   ";
+    for (int i = 1; i <= ncol; i++) {
+        if (i < 10){
+            cout<<" |   "<<i;
+        }
+        else if (i == 10) cout << " | "<<i;
+        else if (i<100) {
+            cout<<"  | "<<i;
+        }
+    }
+    cout<< "  | ----->> PANTALLA(Ubicacion)"<<endl;
+    cout<<aux<<endl;
+
+}
+
+void Pelicula::reservar(string fila,  int col)
+{
+    string aux = "A";
+    int _fila = ((aux[0]+(nfil-1))-fila[0]);     //Numero equivalente de la fila al string ingresado
+    for (int i = 0; i < nfil; i++) {
+        for (int j=0; j< ncol; j++) {
+            if (i == _fila && j==col-1) asientos_sala[i][j] = "-";    //Realiza la reservacion
+        }
+    };
+}
+
--- Asientos_Cine/pelicula.h
+++ Asientos_Cine/pelicula.h
@@ -1,11 +1,57 @@
 #ifndef PELICULA_H
 #define PELICULA_H
 
+#include <string>
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
 
 class Pelicula
 {
 public:
-    Pelicula();
+    Pelicula(string _nombre, string _genero, int _sala, string _hora, int _nfil, int _ncol, string _clasificacion, string _formato);
+    ~Pelicula();
+    void setNombre(string _nombre);
+    void setGenero(string _genero);
+    void setSala(int _sala);
+    void setHora(string _hora);
+    void setAsientDisponible(int _asien_disponible);
+    void setAsientTotal();
+    void setClasificacion(string _clasificacion);
+    void setFilas_Colum(int _nfil, int _ncol);
+    void setAsientosSala();
+
+    string getNombre();
+    string getGenero();
+    string getDuracion();
+    int getSala();
+    string getHora();
+    int getAsientDisponible();
+    int getAsientTotal();
+    string getClasificacion();
+    string getFormato();
+    int getFila();
+    int getColumna();
+
+    void showSala();                //Muestra la sala con los asientos disponibles
+    void reservar(string fila,  int col);          //Realiza la reserva de una asiento
+
+private:
+    //Atributos
+    string nombre;
+    string genero;
+    string duracion;
+    int sala;                              //Numero de la sala
+    string hora;
+    int asien_disponible;
+    int asien_total;
+    string clasificacion;
+    string formato;
+
+    string** asientos_sala;             //Matriz con el estado de los asientos de la sala de cine
+    int nfil, ncol;
 };
 
 #endif // PELICULA_H
